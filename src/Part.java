5204c0a05f48268731d4b9cb6ba5938d8d9ec1fa
leopiccionia <leopiccionia@gmail.com>
2017-04-15 21:07:10 -0300

    Part initialization

--- src/Part.java
+++ src/Part.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
+import java.util.UUID;
 
 public class Part {
 
+	UUID code;
+	String name;
+	String description;
+	ArrayList<PartQuantity> parts;
+	
+	public Part(String name, String description){
+		this.code = UUID.randomUUID();
+		this.name = name;
+		this.description = description;
+		this.parts = new ArrayList<PartQuantity>();
+	}
+	
 }
